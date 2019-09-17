1800db4c10acd6627fe9e96bf48b12bcf95afbdd
Okae Jin <mokafogeru@gmail.com>
2019-09-18 00:40:49 +0900

    upadte: effectmanager

---
+++ Mizukiri/Assets/Assets/Script/Game/Graphic/EffectsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Effect
+{
+	Normal,
+	Perfect
+}
+
+public class EffectsManager : MonoBehaviour
+{
+	GameObject effectObject;
+	GameObject[] effects = new GameObject[2];
+
+	void Awake()
+	{
+		effectObject = Resources.Load<GameObject>("prefabs/effects/Effects");
+		effectObject = Instantiate(effectObject);
+		effects[0] = effectObject.transform.Find("NormalEffect(Clone)").gameObject; // 普通の水パシャ
+		effects[1] = effectObject.transform.Find("PerfectEffect(Clone)").gameObject;
+	}
+
+	public void PlayEffect(Effect type, Vector2 position)
+	{
+		effects[(int) type].transform.position = position;
+		effects[(int) type].GetComponent<ParticleSystem>().Play();
+	}
+
+}
