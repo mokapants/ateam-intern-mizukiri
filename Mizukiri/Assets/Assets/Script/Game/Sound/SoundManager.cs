47c6ed7190ce4d2ab10f4f4c5667998d54ffc5d9
Okae Jin <mokafogeru@gmail.com>
2019-09-22 21:46:14 +0900

    update: 橋本さん変更分

---
+++ Mizukiri/Assets/Assets/Script/Game/Sound/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundEffect
+{
+    Start,
+    Bound,
+    Boost,
+}
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip[] soundEffects;
+
+
+    public AudioClip SetSoundEffect(SoundEffect type)
+    {
+        return soundEffects[(int)type];
+    }
+}
