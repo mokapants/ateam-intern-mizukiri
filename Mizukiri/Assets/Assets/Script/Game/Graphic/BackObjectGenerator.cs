9d406c65ab89946ce21e40086e8fe64dcbee8073
Okae Jin <mokafogeru@gmail.com>
2019-09-24 20:50:47 +0900

    update: 魚が背景で動く

---
+++ Mizukiri/Assets/Assets/Script/Game/Graphic/BackObjectGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackObjectGenerator : MonoBehaviour
+{
+	[SerializeField] GameObject[] fishes;
+	Transform nextWave;
+
+	void Start()
+	{
+		StartCoroutine("Generator");
+	}
+
+	IEnumerator Generator()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1f);
+			if (!GameManager.isStart || GameManager.isEnd)
+			{
+				continue;
+			}
+
+			if (Random.Range(0, 5) < 2)
+			{
+				int fishType = Random.Range(0, 2);
+				Instantiate(fishes[fishType]);
+			}
+		}
+	}
+}
---
+++ Mizukiri/Assets/Assets/Script/Game/Graphic/Fish.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		float x = GameObject.Find("stone(Clone)").transform.position.x + 100;
+		Vector3 position = new Vector3(x, -3.3f, 1);
+		transform.position = position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 newPosition = transform.position;
+		newPosition.x -= 0.01f;
+		transform.position = newPosition;
+
+		float x = GameObject.Find("stone(Clone)").transform.position.x;
+		if (newPosition.x < x - 50)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
